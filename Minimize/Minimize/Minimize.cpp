faa7c9f8fb15bd818994866a06543da4ef329f07
Eugene <cullofdute19762005@gmail.com>
2024-12-17 12:17:45 +0300

    init commit

---
+++ Minimize/Minimize/Minimize.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct MealyAutomata
+{
+    vector<string> states;
+    vector<string> entries;
+    vector<vector<pair<string, string>>> transitions;
+};
+
+struct MooreAutomata
+{
+    vector<pair<string, string>> states;
+    vector<string> entries;
+    vector<vector<string>> transitions;
+};
+
+int main()
+{
+
+}
