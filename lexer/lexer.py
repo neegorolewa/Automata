19f848444d014d641326bc701b6edeba6755fa5d
Eugene <cullofdute19762005@gmail.com>
2025-01-22 13:40:28 +0300

    test

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -4,210 +4,211 @@ import sys
 import re
 from typing import NamedTuple, Optional
 
-class Token(NamedTuple):
-    type: str
-    lexeme: str
-    line: int
-    column: int
+class Token:
+    def __init__(self, type_, lexeme, line, column):
+        self.type = type_
+        self.lexeme = lexeme
+        self.line = line
+        self.column = column
 
     def __str__(self):
-        return f'{self.type} ({self.line}, {self.column}) "{self.lexeme}"'
+        return f"{self.type} ({self.line}, {self.column}) \"{self.lexeme}\""
+
+class Patterns:
+    # Регулярные выражения для токенов
+    WHITESPACE = r"[\s]+"
+    LINE_COMMENT = r"//[^\n]*"
+    BLOCK_COMMENT = r"\{[^}]*\}"
+    STRING = r"'(?:[^']|'')*'"
+    FLOAT = r"\d+(\.)?(\d+)?([eE][+-]?\d+)?"
+    INTEGER = r"\d+"
+    IDENTIFIER = r"[a-zA-Z_][a-zA-Z0-9_]*"
+    OPERATOR = r"[\+\-\*/;,=\(\)\[\]{}<>:\.]"
+    BAD = r"[^ \t\n\(\)\+\-\*/;,=\[\]{}<>'.:a-zA-Z0-9_]+"
+    OPERATOR_CHECK = r"[ \t\(\)\+\-\*/;,=\[\]{}<>'.:]"
+    NOT_RIGHT_SYMBOLS = r"^[a-zA-Z0-9_]*$"
+    NOT_RIGHT_OPERATORS = r"[^ \t\n\(\)\+\-\*/;,=\[\]{}<>'.:]*"
 
 class PascalLexer:
-    def __init__(self, input_file: str):
-        self.input_file = input_file
+    KEYWORDS = {
+        "array": "ARRAY",
+        "begin": "BEGIN",
+        "else": "ELSE",
+        "end": "END",
+        "if": "IF",
+        "of": "OF",
+        "or": "OR",
+        "program": "PROGRAM",
+        "procedure": "PROCEDURE",
+        "then": "THEN",
+        "type": "TYPE",
+        "var": "VAR"
+    }
+
+    OPERATORS = {
+        "*": "MULTIPLICATION",
+        "+": "PLUS",
+        "-": "MINUS",
+        "/": "DIVIDE",
+        ";": "SEMICOLON",
+        ",": "COMMA",
+        "(": "LEFT_PAREN",
+        ")": "RIGHT_PAREN",
+        "[": "LEFT_BRACKET",
+        "]": "RIGHT_BRACKET",
+        "=": "EQ",
+        ">": "GREATER",
+        "<": "LESS",
+        "<=": "LESS_EQ",
+        ">=": "GREATER_EQ",
+        "<>": "NOT_EQ",
+        ":": "COLON",
+        ":=": "ASSIGN",
+        ".": "DOT"
+    }
+
+    def __init__(self, input_file):
+        self.input_file = open(input_file, 'r')
         self.current_line = 0
+        self.current_column = 0
+        self.buffer = ""
+        self.eof = False
+
+    def read_next_line(self):
+        self.buffer = self.input_file.readline()
+
+        if not self.buffer:
+            self.eof = True
+            self.buffer = ""
+        else:
+            self.current_line += 1
         self.current_column = 1
 
-        self.keywords = {
-            "array",
-            "begin",
-            "else",
-            "end",
-            "if",
-            "of",
-            "or",
-            "program",
-            "procedure",
-            "then",
-            "type",
-            "var",
-        }
-
-        self.patterns = [
-            ("WHITESPACE", r"[\s]+"),
-            ("LINE_COMMENT", r"//[^\n]*"),
-            ("BLOCK_COMMENT", r"\{[^}]*\}"),
-            ("STRING", r"'(?:[^']|'')*'"),
-            ("FLOAT", r"\d+\.\d+(?:[eE][-+]?\d+)?|\.\d+(?:[eE][-+]?\d+)?|\d+[eE][-+]?\d+"),
-            ("INTEGER", r"\d+"),
-            ("IDENTIFIER", r"[a-zA-Z_][a-zA-Z0-9_]*"),
-            ("ASSIGN", r":="),
-            ("LESS_EQ", r"<="),
-            ("GREATER_EQ", r">="),
-            ("NOT_EQ", r"<>"),
-            ("MULTIPLICATION", r"\*"),
-            ("PLUS", r"\+"),
-            ("MINUS", r"-"),
-            ("DIVIDE", r"/"),
-            ("SEMICOLON", r";"),
-            ("COMMA", r","),
-            ("LEFT_PAREN", r"\("),
-            ("RIGHT_PAREN", r"\)"),
-            ("LEFT_BRACKET", r"\["),
-            ("RIGHT_BRACKET", r"\]"),
-            ("EQ", r"="),
-            ("GREATER", r">"),
-            ("LESS", r"<"),
-            ("COLON", r":"),
-            ("DOT", r"\."),
-            ("BAD", r"."),
-        ]
-
-        self.regex = re.compile(
-            "|".join(f"(?P<{name}>{pattern})" for name, pattern in self.patterns)
-        )
-    
     def next_token(self):
-        in_block_comment = False
-        block_comment_start_line = 0
-        block_comment_start_column = 0
-        block_comment_content = ""
+        while not self.eof or self.buffer:
+            if not self.buffer:
+                self.read_next_line()
+                if self.eof:
+                    return None
 
-        in_string = False
-        string_start_line = 0
-        string_start_column = 0
-        string_content = ""
+            # Пропуск пробелов
+            while self.buffer and self.buffer[0].isspace():
+                self.buffer = self.buffer[1:]
+                self.current_column += 1
 
-        with open(self.input_file, "r") as file:
-            for line in file:
-                self.current_column = 1
-                self.current_line += 1
+            if not self.buffer:
+                continue
 
-                if in_block_comment:
-                    closing_index = line.find("}")
-                    if closing_index != -1:
-                        in_block_comment = False
-                        self.current_column = closing_index + 2
-                        line = line[closing_index + 1 :]
+            self.current_column += len(self.buffer) - len(self.buffer.lstrip())
+
+            # Обработка комментариев
+            if self.buffer.startswith("//"):
+                self.buffer = ""
+                continue
+
+            if self.buffer.startswith("{"):
+                temp = ""
+                temp += self.buffer.replace("\n", "")
+                start_line = self.current_line
+                start_column = self.current_column
+                while True:
+                    end_index = self.buffer.find("}")
+                    if end_index != -1:
+                        self.buffer = self.buffer[end_index + 1:]
+                        self.current_column += end_index + 1
+                        break
                     else:
-                        block_comment_content += line
-                        continue
-
-                if in_string:
-                    closing_index = line.find("'")
-                    if closing_index != -1:
-                        in_string = False
-                        string_content += line[: closing_index + 1]
-                        yield Token(
-                            "STRING",
-                            string_content,
-                            string_start_line,
-                            string_start_column,
-                        )
-                        self.current_column = closing_index + 2
-                        line = line[closing_index + 1 :]
-                    else:
-                        string_content += line
-                        continue
-
-                for match in self.regex.finditer(line):
-                    token_type = match.lastgroup
-                    lexeme = match.group(token_type)
-
-                    if token_type == "WHITESPACE":
-                        self.current_column += len(lexeme)
-                        continue
-
-                    if token_type == "LINE_COMMENT":
-                        continue
-
-                    if token_type == "BLOCK_COMMENT":
-                        self.current_column += len(lexeme)
-                        continue
-
-                    if lexeme == "{":
-                        in_block_comment = True
-                        block_comment_start_line = self.current_line
-                        block_comment_start_column = self.current_column
-                        block_comment_content = lexeme
-                        closing_index = line.find("}", self.current_column)
-                        if closing_index != -1:
-                            in_block_comment = False
-                            self.current_column = closing_index + 2
-                            continue
+                        self.read_next_line()
+                        if self.eof:
+                            bad_token = Token("BAD", temp, start_line, start_column)
+                            self.buffer = ""
+                            return bad_token
                         else:
-                            block_comment_content += line[self.current_column :]
-                            continue
+                            temp += "\n" + self.buffer.replace("\n", "")
+                continue
 
-                    if in_block_comment:
-                        continue
-
-                    if lexeme == "'":
-                        if not in_string:
-                            in_string = True
-                            string_start_line = self.current_line
-                            string_start_column = self.current_column
-                            string_content = lexeme
-                        else:
-                            in_string = False
-                            string_content += lexeme
-                            yield Token(
-                                "STRING",
-                                string_content,
-                                string_start_line,
-                                string_start_column,
-                            )
-                            self.current_column += len(lexeme)
-                            continue
-
-                    if in_string:
-                        string_content += lexeme
-                        self.current_column += len(lexeme)
-                        continue
-
-                    if token_type == "IDENTIFIER":
-                        if len(lexeme) > 256:
-                            yield Token(
-                                "BAD", lexeme, self.current_line, self.current_column
-                            )
-                            self.current_column += len(lexeme)
-                            continue
-
-                        if lexeme.lower() in self.keywords:
-                            token_type = lexeme.upper()
-
-                    if token_type == "INTEGER":
-                        if len(lexeme) > 16:
-                            yield Token(
-                                "BAD", lexeme, self.current_line, self.current_column
-                            )
-                            self.current_column += len(lexeme)
-                            continue
-
-                    yield Token(
-                        token_type,
-                        lexeme,
-                        self.current_line,
-                        self.current_column
-                    )
+            # Обработка строк
+            if self.buffer.startswith("'"):
+                string_start = self.buffer
+                match = re.match(Patterns.STRING, self.buffer)
+                if match:
+                    lexeme = match.group(0)
+                    token = Token("STRING", lexeme, self.current_line, self.current_column)
+                    self.buffer = self.buffer[len(lexeme):]
                     self.current_column += len(lexeme)
+                    return token
+                else:
+                    string_start = string_start.replace("\n", "")
+                    bad_token = Token("BAD", string_start, self.current_line, self.current_column)
+                    self.buffer = ""
+                    return bad_token
 
-            if in_block_comment:
-                yield Token(
-                    "BAD",
-                    block_comment_content,
-                    block_comment_start_line,
-                    block_comment_start_column,
-                )
+            # Обработка чисел
+            match = re.match(Patterns.FLOAT, self.buffer)
+            if match:
+                lexeme = match.group(0)
 
-            if in_string:
-                yield Token(
-                    "BAD", 
-                    string_content, 
-                    string_start_line, 
-                    string_start_column
-                )
+                if "." in lexeme and match.group(2) is None:  # Некорректный float
+                    bad_token = Token("BAD", lexeme, self.current_line, self.current_column)
+                    self.buffer = self.buffer[len(lexeme):]
+                    self.current_column += len(lexeme)
+                    return bad_token
+
+                if lexeme.isdigit() and len(lexeme) > 16:  # Некорректный integer
+                    bad_token = Token("BAD", lexeme, self.current_line, self.current_column)
+                    self.buffer = self.buffer[len(lexeme):]
+                    self.current_column += len(lexeme)
+                    return bad_token
+
+                token_type = "FLOAT" if "." in lexeme or "e" in lexeme.lower() else "INTEGER"
+                token = Token(token_type, lexeme, self.current_line, self.current_column)
+                self.buffer = self.buffer[len(lexeme):]
+                self.current_column += len(lexeme)
+                return token
+
+            # Обработка идентификаторов и ключевых слов
+            match = re.match(Patterns.IDENTIFIER, self.buffer)
+            if match:
+                lexeme = match.group(0)
+
+                if len(lexeme) > 256:
+                    bad_token = Token("BAD", lexeme, self.current_line, self.current_column)
+                    self.buffer = self.buffer[len(lexeme):]
+                    self.current_column += len(lexeme)
+                    return bad_token
+
+                token_type = self.KEYWORDS.get(lexeme.lower(), "IDENTIFIER")
+                token = Token(token_type, lexeme, self.current_line, self.current_column)
+                self.buffer = self.buffer[len(lexeme):]
+                self.current_column += len(lexeme)
+                
+                self.buffer = self.buffer.replace("\n", "")
+                
+                if self.buffer and not re.match(Patterns.OPERATOR_CHECK, self.buffer[0]) and not re.match(Patterns.NOT_RIGHT_SYMBOLS, self.buffer):
+                    self.current_column -= len(lexeme)
+                    bad_token = Token("BAD", lexeme + self.buffer, self.current_line, self.current_column)
+                    self.buffer = ""
+                    return bad_token
+                
+                return token
+
+            # Обработка операторов
+            for op, token_type in sorted(self.OPERATORS.items(), key=lambda x: -len(x[0])):
+                if self.buffer.startswith(op):
+                    token = Token(token_type, op, self.current_line, self.current_column)
+                    self.buffer = self.buffer[len(op):]
+                    self.current_column += len(op)
+                    return token
+
+            # Обработка некорректных лексем
+            if not re.match(Patterns.NOT_RIGHT_SYMBOLS, self.buffer):
+                match = re.match(Patterns.NOT_RIGHT_OPERATORS, self.buffer)
+                self.buffer = self.buffer[len(match.group(0)):]
+                bad_token = Token("BAD", match.group(0), self.current_line, self.current_column)
+                self.current_column += len(match.group(0))
+                return bad_token
+
+        return None
 
 def main():
     if len(sys.argv) != 3:
@@ -219,10 +220,13 @@ def main():
 
     lexer = PascalLexer(input_file)
 
-    with open(output_file, "w") as output:
-        for token in lexer.next_token():
+    with open(output_file, 'w') as output:
+        while True:
+            token = lexer.next_token()
+            if token is None:
+                break
             print(token)
-            output.write(str(token) + "\n")
+            output.write(str(token) + '\n')
 
 if __name__ == "__main__":
     main()
